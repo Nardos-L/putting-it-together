7ff283727a54be2bc3e5c7c789a9425085652a30
Nardos Lengo <nardi.8199@gmail.com>
2021-07-07 20:17:27 -0700

    First commit

---
+++ src/components/PersonCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+class PersonCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age 
+        };
+    }
+
+    increaseAge = () => {
+        this.setState({
+            age: this.state.age + 1
+        });
+    }
+    render() {
+        return (
+            <div>
+            <h1>{ this.props.firstName }, { this.props.lastName }</h1>
+            <p>Age: { this.state.age }</p>
+            <p>Hair Color: { this.props.hairColor }</p>
+            <button onClick={this.increaseAge}>Birthday Button for { this.props.lastName } { this.props.firstName }  </button>
+            </div>
+        );
+    }
+}
+
+export default PersonCard;
